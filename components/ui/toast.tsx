668cfd0a55638df3d3d9b0d54d0a24fd0532e527
DangHieu19022004 <danghieu19122004@gmail.com>
2026-01-13 18:47:29 +0700

    hover pointer close toast cpn

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -78,7 +78,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      'absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-100 transition-colors hover:text-foreground focus:outline-none focus:ring-2 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.coming-soon]:text-pink-400 group-[.coming-soon]:hover:text-pink-600 group-[.coming-soon]:focus:ring-pink-300 group-[.coming-soon]:focus:ring-offset-pink-100',
+      'absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-100 transition-colors hover:text-foreground focus:outline-none focus:ring-2 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.coming-soon]:text-pink-400 group-[.coming-soon]:hover:text-pink-600 group-[.coming-soon]:focus:ring-pink-300 group-[.coming-soon]:focus:ring-offset-pink-100 hover:cursor-pointer',
       className,
     )}
     toast-close=""
